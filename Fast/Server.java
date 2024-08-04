ec0d89138a7ed354aef52116ce3ec952114055f6
Shru Ku <1ms22cs126@msrit.edu>
2024-08-04 20:23:31 +0530

    Update Server.java

--- Fast/Server.java
+++ Fast/Server.java
@@ -5,9 +5,7 @@ public class Server {
     public static void main(String[] args) throws Exception {
 
         ServerSocket sersock = new ServerSocket(4000);
-        System.out.println("Server ready for connection");
         Socket s = sersock.accept();
-        System.out.println("Connection Is successful and waiting for chatting");
 
         InputStream i = s.getInputStream();
         BufferedReader r1 = new BufferedReader(new InputStreamReader(i));
