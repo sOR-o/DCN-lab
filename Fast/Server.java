c0066f6020756b077516c98b5e89e163a29c7330
Shru Ku <1ms22cs126@msrit.edu>
2024-08-04 20:22:05 +0530

    Update Server.java

--- Fast/Server.java
+++ Fast/Server.java
@@ -6,25 +6,25 @@ public class Server {
 
         ServerSocket sersock = new ServerSocket(4000);
         System.out.println("Server ready for connection");
-        Socket sock = sersock.accept();
+        Socket s = sersock.accept();
         System.out.println("Connection Is successful and waiting for chatting");
 
-        InputStream istream = sock.getInputStream();
-        BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+        InputStream i = s.getInputStream();
+        BufferedReader r1 = new BufferedReader(new InputStreamReader(i));
         String fname = fileRead.readLine();
 
         
-        BufferedReader ContentRead = new BufferedReader(new FileReader(fname));
-        OutputStream ostream = sock.getOutputStream();
-        PrintWriter pwrite = new PrintWriter(ostream, true);
+        BufferedReader r2 = new BufferedReader(new FileReader(fname));
+        OutputStream o = s.getOutputStream();
+        PrintWriter p = new PrintWriter(o, true);
         String str;
-        while ((str = ContentRead.readLine()) != null) {
-            pwrite.println(str);
+        while ((str = r2.readLine()) != null) {
+            p.println(str);
         }
-        sock.close();
+        s.close();
         sersock.close();
-        pwrite.close();
-        fileRead.close();
-        ContentRead.close();
+        p.close();
+        r1.close();
+        r2.close();
     }
 }
